4a9c5d87a9f96341f73037ff4fce4a5149d22b84
RAJENDRA YADAV <rjyadav0230@gmail.com>
2023-02-25 17:54:33 +0530

    Todo styling done

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Todo } from "../model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import styles from "../css/singletodo.module.css";
 interface Props {
   todo: Todo;
   todos: Todo[];
@@ -7,12 +10,18 @@ interface Props {
 }
 const SingleTodo: React.FC<Props> = ({ todo, todos, setTodos }: Props) => {
   return (
-    <form className="singleTodo">
-      <span className="singleTodoText">{todo.todo}</span>
+    <form className={styles.singleTodo}>
+      <span className={styles.singleTodoText}>{todo.todo}</span>
       <div>
-        <span className="icon"></span>
-        <span className="icon"></span>
-        <span className="icon"></span>
+        <span className={styles.icon}>
+          <AiFillEdit />
+        </span>
+        <span className={styles.icon}>
+          <AiFillDelete />
+        </span>
+        <span className={styles.icon}>
+          <MdDone />
+        </span>
       </div>
     </form>
   );
